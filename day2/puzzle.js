f47aae0f13b6575dd26df1c8dcaaa0d32310513e
Venkata Phaneendra Andukuri <venkataphaneendraandukuri@Venkatas-MacBook-Pro.local>
2021-03-26 20:11:32 +0530

    day2 puzzle2 done

--- day2/puzzle.js
+++ day2/puzzle.js
@@ -2,24 +2,17 @@ const fs = require('fs');
 
 const entries = fs.readFileSync('./input.txt', 'utf-8').split('\n');
 
-const getCharCount = function (string, alphabet) {
-  let count = 0;
-  const list = string.split('');
-  list.forEach((letter) => {
-    count += letter === alphabet ? 1 : 0;
-  });
-  return count;
-};
-
 const getValidPasswords = (entries) => {
   const validPasswords = [];
   entries.forEach((entry) => {
     const [letterOccurrences, word] = entry.split(':');
     const [range, letter] = letterOccurrences.split(' ');
-    const [down, up] = range.split('-').map(Number);
+    const [first, second] = range.split('-').map(Number);
 
-    const letterCount = getCharCount(word, letter);
-    if (letterCount >= down && letterCount <= up) {
+    const isValid =
+      (word[first] === letter || word[second] === letter) &&
+      word[first] !== word[second];
+    if (isValid) {
       validPasswords.push(word);
     }
   });
